7e293bf9d9dd8739de9dd32c6956db57a0f0b57a
turtlecabbage-juno <householdappliance@metalfan.com>
2025-09-16 11:17:31 -0400

    added my neovim config to make it easier to start

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,22 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  { import = "astrocommunity.file-explorer.mini-files" },
+  { import = "astrocommunity.git.nvim-tinygit" },
+  { import = "astrocommunity.icon.mini-icons" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.media.neocord-nvim" },
+  { import = "astrocommunity.motion.mini-basics" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
+  { import = "astrocommunity.pack.nim" },
+}
